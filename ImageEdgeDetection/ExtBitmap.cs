23d915e964019bce4b222b8910c63175b37ce1d4
Jon <joaquim.jonathan@bluewin.ch>
2017-12-09 09:22:21 +0100

    test3

--- ImageEdgeDetection/ExtBitmap.cs
+++ ImageEdgeDetection/ExtBitmap.cs
@@ -11,7 +11,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using System.Drawing.Drawing2D;
-//test jon
+
 namespace ImageEdgeDetection
 {
     public static class ExtBitmap
